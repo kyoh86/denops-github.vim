494e834c8c9850ec416fd8d9c95a8cdc5fa1fe02
GitHub <noreply@github.com>
2025-03-19 09:06:58 +0000

    chore(deps): Update deno dependencies

--- denops/github/client.ts
+++ denops/github/client.ts
@@ -3,11 +3,11 @@ import { Octokit as OctokitCore } from "npm:@octokit/core@6.1.4";
 import {
   type Api,
   restEndpointMethods,
-} from "npm:@octokit/plugin-rest-endpoint-methods@13.3.1";
+} from "npm:@octokit/plugin-rest-endpoint-methods@13.5.0";
 import {
   type PaginateInterface,
   paginateRest,
-} from "npm:@octokit/plugin-paginate-rest@11.4.3";
+} from "npm:@octokit/plugin-paginate-rest@11.6.0";
 import {
   paginateGraphQL,
   type paginateGraphQLInterface,
@@ -17,7 +17,7 @@ export const Octokit = OctokitCore
   .plugin(restEndpointMethods)
   .plugin(paginateRest)
   .plugin(paginateGraphQL);
-import { createOAuthDeviceAuth } from "npm:@octokit/auth-oauth-device@7.1.3";
+import { createOAuthDeviceAuth } from "npm:@octokit/auth-oauth-device@7.1.4";
 import { systemopen } from "jsr:@lambdalisue/systemopen@~1.0.0";
 
 type Verification = {
